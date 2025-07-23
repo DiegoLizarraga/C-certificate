29c40ba22ec3c69cd5d1a5ec4e1b95154ccfaad9
Diego Martin Lizarraga Sanchez <lizarragasanchezdiego@gmail.com>
2025-07-23 05:35:50 +0000

    Area square

---
+++ Calculate the Area of a Square.cpp
@@ -0,0 +1,12 @@
+#include <iostream> 
+using namespace std; 
+int main() { 
+    int side;
+     cout << "Please Enter A Side of Square" << endl; 
+    cin >> side; 
+    int area = side * side;
+     cout << "The Area Of Square Is " << area << endl;
+      
+ return 0;   
+    } 
+    
